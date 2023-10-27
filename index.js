63ae3d3c5d3a80972d0e0f0036b76f70c85b23cf
AbdulWahidKhan315 <k.wahidkhan55@gmail.com>
2023-10-28 03:24:09 +0600

    added code for generate toke with jwt and verify the token with a middleware verify finction

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -20,6 +26,23 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyToken = async(req,res,next)=>{
+    const token =req.cookies?.token;
+    // console.log('token from verify token',token)
+    if(!token){
+        return res.status(401).send({message: 'not authorized'})
+    }
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded) => {
+        if(err){
+            console.log(err);
+            return res.status(401).send({message: 'unauthorized'})
+        }
+        // console.log('value in the token',decoded);
+        req.user=decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -29,7 +52,21 @@ async function run() {
         const serviceCollection = client.db('carDoctor').collection('services');
         const bookingsCollection = client.db('carDoctor').collection('bookings');
 
+        //auth related api
+        app.post('/jwt',(req,res)=>{
+            const user = req.body;
+            const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'});
+            res
+            .cookie('token',token,{
+                httpOnly: true,
+                secure: false
+            })
+            .send({success: true})
 
+        })
+
+
+        //booking related api
         app.get('/services',async(req,res)=>{
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
@@ -48,8 +85,13 @@ async function run() {
         
         //Booking collectin...
 
-        app.get('/bookings',async(req,res)=>{
+        app.get('/bookings',verifyToken,async(req,res)=>{
             let query = {};
+            // console.log(req.cookies.token)
+            // console.log('user in the valid token',req.user)
+            if(req.query?.email !== req.user.email){
+                return res.status(403).send({message: 'forbidden access'})
+            }
             if(req.query?.email){
                 query = {email: req.query.email}
             }
